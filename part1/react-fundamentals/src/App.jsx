60702917e04e847ac4b37bbb7e29238590fd66a5
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-22 20:42:33 -0500

    refactored 1.2

---
+++ part1/react-fundamentals/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exerciseCount}
+      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.count}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { name : 'Fundamentals of React', exerciseCount : 10 }, 
+    { name : 'Using props to pass data', exerciseCount : 7 }, 
+    { name : 'State of a component', exerciseCount: 14 }, 
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total count={parts[0].exerciseCount + parts[1].exerciseCount + parts[2].exerciseCount} />
+    </div>
+  )
+}
+
+export default App
+
