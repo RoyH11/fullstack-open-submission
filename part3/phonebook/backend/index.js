ad1a501642cdc7a0d3a202069d1ab068db78ea3e
RoyH11 <ruoqiuhuang@gmail.com>
2025-05-12 12:46:01 -0500

    exercises 3.1-3.6

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -34,12 +34,57 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 }) 
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(p => p.id === id) 
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
     const date = new Date()
     const info = `<p>Phonebook has info for ${persons.length} people</p>`
     const dateInfo = `<p>${date}</p>`
     response.send(info + dateInfo)
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+}) 
+
+// ID generation function
+const generateId = () => {
+    return Math.floor(Math.random() * 10000).toString()
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: 'name or number missing' })
+    }
+
+    if (persons.some(p => p.name === body.name)) {
+        return response.status(400).json({ error: 'name must be unique' })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+}) 
+
 
 const PORT = 3001
 app.listen(PORT, () => {
