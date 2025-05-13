4830170a13f83410064434530528ea54e9bd6d2a
RoyH11 <ruoqiuhuang@gmail.com>
2025-05-13 16:12:03 -0500

    endpoint handler and port assignment

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -103,8 +103,13 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 }) 
 
+// unknown endpoint handler
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
