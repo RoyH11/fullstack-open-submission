c9ca96087c142759add212ef8840e451d7e588fc
RoyH11 <ruoqiuhuang@gmail.com>
2025-05-23 17:32:07 -0500

    use middleware to handle error

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -33,7 +33,7 @@ app.get('/api/notes', (request, response) => {
 })
 
 // get a note by id
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -42,10 +42,7 @@ app.get('/api/notes/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+        .catch(error => next(error))
 })
 
 // add a new note
@@ -80,8 +77,19 @@ app.delete('/api/notes/:id', (request, response) => {
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+// error handler
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
