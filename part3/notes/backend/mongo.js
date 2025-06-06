42a569da0f724b1b011b2afe98823e56ce8408b3
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-06 00:18:56 -0500

    lint

--- part3/notes/backend/mongo.js
+++ part3/notes/backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) { 
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -19,8 +19,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String, 
-    important: Boolean,
+  content: String, 
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -36,8 +36,8 @@ const Note = mongoose.model('Note', noteSchema)
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
