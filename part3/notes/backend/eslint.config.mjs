bbcf02e590977bf0d38efc0d58cd328537c33e80
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-06 00:20:12 -0500

    eslint config

---
+++ part3/notes/backend/eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals"
+import js from "@eslint/js"
+import stylisticJs from '@stylistic/eslint-plugin'
+
+export default [
+  js.configs.recommended,
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "commonjs", 
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    }, 
+    plugins: {
+      '@stylistic': stylisticJs,
+    }, 
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      //TODO: more 
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  }
+]
