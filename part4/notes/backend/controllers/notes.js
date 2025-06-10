971c997561e3b509b507cc724a819d720aa28603
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-10 16:20:16 -0500

    refactor

--- part4/notes/backend/controllers/notes.js
+++ part4/notes/backend/controllers/notes.js
@@ -1,36 +1,38 @@
 const notesRouter = require('express').Router()
-const Note = require('.../models/note')
+const Note = require('../models/note')
 
 notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
+  Note.find({}).then((notes) => {
     response.json(notes)
   })
 })
 
 notesRouter.get('/:id', (request, response, next) => {
   Note.findById(request.params.id)
-    .then(note => {
+    .then((note) => {
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 notesRouter.post('/', (request, response, next) => {
   const body = request.body
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
+  note
+    .save()
+    .then((savedNote) => {
       response.json(savedNote)
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
@@ -38,14 +40,14 @@ notesRouter.delete('/:id', (request, response, next) => {
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 notesRouter.put('/:id', (request, response, next) => {
   const { content, important } = request.body
 
   Note.findById(request.params.id)
-    .then(note => {
+    .then((note) => {
       if (!note) {
         return response.status(404).end()
       }
@@ -57,7 +59,7 @@ notesRouter.put('/:id', (request, response, next) => {
         response.json(updatedNote)
       })
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 module.exports = notesRouter
