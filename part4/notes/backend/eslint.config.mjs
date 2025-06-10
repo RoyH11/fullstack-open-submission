73e433e2fb10b9678a373a14ecdc2a2d96a68358
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-10 15:41:23 -0500

    lint update

--- part4/notes/backend/eslint.config.mjs
+++ part4/notes/backend/eslint.config.mjs
@@ -1,19 +1,21 @@
-import globals from "globals"
-import js from "@eslint/js"
+import globals from 'globals'
+import js from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin'
 
 export default [
   js.configs.recommended,
-  { 
-    files: ["**/*.js"], 
-    languageOptions: { 
-      sourceType: "commonjs", 
-      globals: { ...globals.node },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
       ecmaVersion: 'latest',
-    }, 
+    },
     plugins: {
       '@stylistic': stylisticJs,
-    }, 
+    },
     rules: {
       '@stylistic/indent': ['error', 2],
       '@stylistic/linebreak-style': ['error', 'unix'],
@@ -23,10 +25,10 @@ export default [
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
       'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'off', 
+      'no-console': 'off',
     },
   },
   {
     ignores: ['dist/**'],
-  }
+  },
 ]
