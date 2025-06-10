b7ba4cfd73e0d8d88e5937c37ec8339249ce313b
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-10 15:04:05 -0500

    lint and lint fix for phonebook

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -6,42 +6,42 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to ', url)
 mongoose
-    .connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 3,
-    },
-    number: {
-        type: String,
-        required: true,
-        minlength: 8,
-        validate: {
-            validator: function(v) {
-                // regex: 2 or 3 digits, dash, at least 1 digit
-                return /^(\d{2,3}-\d+)$/.test(v);
-            }, 
-            message: props => 
-                `${props.value} is not a valid phone number! It should be in the format 'XX-XXXXX' or 'XXX-XXXX'`
-        }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        // regex: 2 or 3 digits, dash, at least 1 digit
+        return /^(\d{2,3}-\d+)$/.test(v)
+      },
+      message: props =>
+        `${props.value} is not a valid phone number! It should be in the format 'XX-XXXXX' or 'XXX-XXXX'`
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
