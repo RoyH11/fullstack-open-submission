aa359c5aa1128d6b4374d453d4f331b0040cf4a4
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-12 13:12:18 -0500

    4.1:

--- part4/blog-list/backend/index.js
+++ part4/blog-list/backend/index.js
@@ -1,14 +1,38 @@
-// index.js
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
 const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
