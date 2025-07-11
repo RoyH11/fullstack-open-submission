0e8aa499cdad6def420073a1417066bbfc4f8aa0
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-11 14:29:38 -0500

    only tests

--- part4/notes/backend/tests/note_api.test.js
+++ part4/notes/backend/tests/note_api.test.js
@@ -26,15 +26,15 @@ beforeEach(async () => {
   await noteObject.save()
 })
 
-test('notes are returned as json', async () => {
+test.only('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test('all notes are returned', async () => {
-  const response = await api.get('api/notes')
+test.only('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
 
   assert.strictEqual(response.body.length, initialNotes.length)
 })
