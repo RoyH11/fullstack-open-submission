80d449e6507d295813a6cdbec6e7614f3287ab40
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-16 17:16:33 -0500

    4.9 refactor

--- part4/blog-list/backend/tests/blog_api.test.js
+++ part4/blog-list/backend/tests/blog_api.test.js
@@ -3,7 +3,6 @@ const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const test_blogs = require('./test_blogs')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
@@ -11,8 +10,9 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  await Blog.insertMany(test_blogs.blogs)
+  await Blog.insertMany(helper.initialBlogs)
 })
+
 
 test('blogs are returned as json', async () => {
   await api
@@ -21,22 +21,25 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, test_blogs.blogs.length)
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
+
 
 test('blog posts have id property instead of _id', async () => {
   const response = await api.get('/api/blogs')
 
   const blogs = response.body
   assert(blogs.length > 0) // Ensure there are blogs to check
-  
+
   blogs.forEach(blog => {
     assert(blog.id !== undefined, 'Blog should have id property')
     assert(blog._id === undefined, 'Blog should not have _id property')
   })
 })
+
 
 after(async () => {
   await mongoose.connection.close()
